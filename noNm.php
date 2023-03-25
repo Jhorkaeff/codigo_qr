ba0e35fa0cd2f90d1aa31759a2c4b271df2d0223
Jhorkaeff <jhorkaeffperaza@hotmail.com>
2023-03-25 04:41:25 -0500

    Update noNm.php

--- noNm.php
+++ noNm.php
@@ -7,5 +7,9 @@
       <p>The music is</p>
       <p>can´t sleep</p>
       <embed src="can't_sleep.mp3" loop="true" autostart="true" width="2" height="0">
-   </body>
+        <audio controls autoplay muted>
+            <source src="can't_sleep.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+    </body>
 </html>
