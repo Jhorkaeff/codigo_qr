5df1e8b4975e3a0cf0bced20674655a6221da963
Khrizalid202 <igniadragneel100@gmail.com>
2023-03-26 21:53:25 -0500

    ñ

--- template/Cabacera.php
+++ template/Cabacera.php
@@ -9,10 +9,12 @@
     </head>
     <body>
         <header>
+        <nav>
             <a href="#" class="logo">Logo</a>
             <ul>
                 <li><a href="Index.php">Inicio</a></li>
                 <li><a href="Index.php">Crear QR</a></li>
                 <li><a href="Cargar.php">Cargar QR</a></li>
             </ul>
+        </nav>
         </header>
