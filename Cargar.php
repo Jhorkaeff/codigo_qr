5835452d9d7255b665fb9838998d2ce674564e38
Khrizalid202 <igniadragneel100@gmail.com>
2023-03-28 12:38:34 -0500

    ok

--- Cargar.php
+++ Cargar.php
@@ -7,9 +7,9 @@
 		<form action="car.php" method="post" enctype="multipart/form-data">
 			<div class = "col-sm-8">
 					<h3>DBQR</h3>
-					<h4>IMPORTANTE: <span>Agregue los datos del estudiante</span></h4>
-					<li>Calidad H de imagen: <p>se le dara la mas alta calidad posible a las imagenes qr</p></li>
-					<li>Tamaño 10: <span><font color="white">Calidad H de imagen: </font>el tamaño proporcional que se le dara a las qr</span></li>
+					<h4>IMPORTANTE: <span><font color="white">Agregue los datos del estudiante</font></span></h4>
+					<li>Calidad H de imagen: <span><font color="white">se le dara la mas alta calidad posible a las imagenes qr</font></span></li>
+					<li>Tamaño 10: <span><font color="white">Calidad H de imagen: el tamaño proporcional que se le dara a las qr</font></span></li>
 					<p>Nivel: H</p>
 					<p>Tamaño: 10</p>
 					<div class="form-group">
