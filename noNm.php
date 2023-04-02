2201f1a73d6a8559f302dfe0bfefb776a3087d3f
Jhorkaeff <jhorkaeffperaza@hotmail.com>
2023-04-02 16:41:12 -0500

    z

--- noNm.php
+++ noNm.php
@@ -1,5 +1,4 @@
 <?php
-    $path = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
-    $path .=$_SERVER["SERVER_NAME"]. dirname($_SERVER["PHP_SELF"]);        
-    echo $path;
+$k = "Kevin Jhorkaeff ";
+echo strtolower(str_replace(' ', '', $k));
 ?>
